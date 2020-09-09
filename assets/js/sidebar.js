406b663c025bd0335be16ee6be3399fbbb078bb6
Saloni Banerjee <salonibanerjee97@gmail.com>
2020-09-09 21:20:18 +0530

    hides sidebar for small devices

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,7 +1,21 @@
 var mini = true;
 
+function closeSidebar() {
+  
+    console.log("closing smol sidebar");
+    document.getElementById("mySidebar").style.width = "1px";
+    document.getElementById("main").style.marginLeft = "1px";
+    this.mini = true;
+  
+}
+
+
 function toggleSidebar() {
-  if (mini) {
+
+  /*if((document.getElementById("mySidebar").style.width == "0.1px") OR (document.getElementById("mySidebar").style.width == "200px"))
+    togglesmolSidebar();
+
+  else*/ if (mini) {
     console.log("opening sidebar");
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
@@ -13,3 +27,4 @@ function toggleSidebar() {
     this.mini = true;
   }
 }
+
